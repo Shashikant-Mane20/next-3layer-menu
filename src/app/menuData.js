722e9dadb5333f8213eb9bdbde133bb1591704ca
[shashikant] <[shashikant.mane801@gmail.com]>
2024-12-04 13:14:46 +0530

    Changes

--- src/app/menuData.js
+++ src/app/menuData.js
@@ -1,66 +1,73 @@
-// app/menuData.js
 export const menuData = [
     {
       name: "Publications",
+      image: "/images/1920x1080.jpg",
       submenu: [
         {
           name: "Newsletters",
+          image: "/images/1920x1080.jpg",
           submenu: [
-            { name: "2023 Editions" },
-            { name: "2022 Editions" },
-            { name: "Archived Editions" },
+            { name: "2023 Editions", image: "/images/1920x1080.jpg" },
+            { name: "2022 Editions", image: "/images/1920x1080.jpg" },
+            { name: "Archived Editions", image: "/images/1920x1080.jpg" },
           ],
         },
         {
           name: "White Papers",
+          image: "/images/1920x1080.jpg",
           submenu: [
-            { name: "Technology White Papers" },
-            { name: "Industry Reports" },
+            { name: "Technology White Papers", image: "/images/1920x1080.jpg" },
+            { name: "Industry Reports", image: "/images/1920x1080.jpg" },
           ],
         },
-        { name: "Brochures" },
-        { name: "Books" },
-        { name: "Blogs" },
-        { name: "Case Studies" },
+        { name: "Brochures", image: "/images/1920x1080.jpg" },
+        { name: "Books", image: "/images/1920x1080.jpg" },
+        { name: "Blogs", image: "/images/1920x1080.jpg" },
+        { name: "Case Studies", image: "/images/1920x1080.jpg" },
       ],
     },
     {
       name: "Industries",
+      image: "/images/1920x1080.jpg",
       submenu: [
         {
           name: "Agriculture",
+          image: "/images/1920x1080.jpg",
           submenu: [
-            { name: "Farm Management" },
-            { name: "Sustainability Solutions" },
+            { name: "Farm Management", image: "/images/1920x1080.jpg" },
+            { name: "Sustainability Solutions", image: "/images/1920x1080.jpg" },
           ],
         },
         {
           name: "Education",
+          image: "/images/1920x1080.jpg",
           submenu: [
-            { name: "E-learning Platforms" },
-            { name: "School Management Systems" },
+            { name: "E-learning Platforms", image: "/images/1920x1080.jpg" },
+            { name: "School Management Systems", image: "/images/1920x1080.jpg" },
           ],
         },
-        { name: "Healthcare" },
-        { name: "Smart City" },
+        { name: "Healthcare", image: "/images/1920x1080.jpg" },
+        { name: "Smart City", image: "/images/1920x1080.jpg" },
       ],
     },
     {
       name: "Services",
+      image: "/images/1920x1080.jpg",
       submenu: [
         {
           name: "AI & Allied Services",
+          image: "/images/1920x1080.jpg",
           submenu: [
-            { name: "Machine Learning" },
-            { name: "Deep Learning" },
-            { name: "Natural Language Processing" },
+            { name: "Machine Learning", image: "/images/1920x1080.jpg" },
+            { name: "Deep Learning", image: "/images/1920x1080.jpg" },
+            { name: "Natural Language Processing", image: "/images/1920x1080.jpg" },
           ],
         },
-        { name: "Data & Analytics" },
-        { name: "Emerging Technologies" },
+        { name: "Data & Analytics", image: "/images/1920x1080.jpg" },
+        { name: "Emerging Technologies", image: "/images/1920x1080.jpg" },
       ],
     },
-    { name: "Contact Us", submenu: null },
-    { name: "About Us", submenu: null },
+    { name: "Contact Us", image: "/images/1920x1080.jpg", submenu: null },
+    { name: "About Us", image: "/images/1920x1080.jpg", submenu: null },
   ];
   
